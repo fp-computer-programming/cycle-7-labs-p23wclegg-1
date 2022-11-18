e70afc9a8039f870bc551464111531ebd467d1e4
William <p23wclegg@fairfieldprep.org>
2022-11-18 11:22:12 -0500

    Functions are the truth

---
+++ Lab_7-1.py
@@ -0,0 +1,7 @@
+#Lab_7-1
+
+def greeting():
+    '''Allows you to say anything'''
+    return("Hello World")
+
+print(greeting())
---
+++ Lab_7-2.py
@@ -0,0 +1,11 @@
+#Lab_7-2
+
+def find_sum(num1, num2):
+    '''Adds the two numbers together for the sum'''
+    num_sum=num1 + num2
+    return num_sum
+#making a function to return the sum
+
+my_sum=find_sum(111, 222)
+print(my_sum)
+#Running the program gives me the sum of the two numbers
