02f92af8928035a9038e4525be9e479fd41e1f3c
William <p23wclegg@fairfieldprep.org>
2022-12-02 08:47:46 -0500

    labs

--- Lab_7-3.py
+++ Lab_7-3.py
@@ -1,4 +1,4 @@
-#Lab_7-1
+#Lab_7-3
 
 def greeting():
     '''Allows you to say anything'''
@@ -7,3 +7,9 @@ def greeting():
 
 print(greeting())
 #print statement for the function
+
+#TEST FUNCTION LIST
+print(greeting("Greetings world"))
+print(greeting("anything else"))
+print(greeting("no greetings"))
+print(greeting("functions are hard"))
